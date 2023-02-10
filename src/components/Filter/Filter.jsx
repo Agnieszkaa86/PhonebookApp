6c9a29720ef172c4919d55198aac0e43b9fc0fd6
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-10 10:50:23 +0100

    Update Filter.jsx

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter } from '../../redux/filterSlice';
+import { selectFilter } from 'redux/selectors';
+import { Input, Label } from './Filter.styled';
+
+export const Filter = () => {
+    const filter = useSelector(selectFilter);
+    const dispatch = useDispatch();
+
+    const handleChange = e => {
+        dispatch(changeFilter(e.currentTarget.value));
+    };
+
+    return (
+        <Label>
+            Find contacts by name
+            <Input value={filter} type="text" onChange={handleChange} />
+        </Label>
+    );
+}
