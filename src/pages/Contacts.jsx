62a9eead03dd62cef8a3be0689a373292493544d
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-10 22:17:05 +0100

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,11 @@
 import { ContactList } from 'components/ContactList/ContactList';
-import { Form } from 'components/Form/Form';
+import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 
 export const Contacts = () => {
   return (
     <div style={{ display: 'block', margin: '20px 80px' }}>
-      <Form />
+      <ContactForm />
       <Filter />
       <ContactList />
     </div>
