cbb720249b89637f2408077cda783cd37a63d924
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-16 00:00:17 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,35 @@
-import { useState } from 'react';
+
 import { useDispatch } from 'react-redux';
 
-import { login } from 'redux/auth/auth-operations';
+import { logIn } from 'redux/auth/auth-operations';
 import { Container, Form, Label, Input, Button, Title } from './Auth.styled';
+
 
 export const Login = () => {
   const dispatch = useDispatch();
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-
-  const handleChange = evt => {
-    const { name, value } = evt.target;
-
-    switch (name) {
-      case 'email':
-        setEmail(value);
-        break;
-
-      case 'password':
-        setPassword(value);
-        break;
-
-      default:
-        alert('Error happened. Please try again');
-        break;
-    }
-  };
 
   const handleSubmit = evt => {
     evt.preventDefault();
-    dispatch(
-      login({
-        email,
-        password,
-      })
-    );
+        const form = evt.target;
+    const email = form.email.value;
+    const password = form.password.value;
+      if ((email && password) === '') {
+      return
+    } else {
+      dispatch(logIn({ email, password }));
+      form.reset();
+    }
 
-    setEmail('');
-    setPassword('');
   };
   return (
     <Container>
       <Title>Log in your account</Title>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} autoComplete="off">
         <Label>
           Email
           <Input
             type="email"
             name="email"
-            onChange={handleChange}
-            value={email}
           />
         </Label>
         <Label>
@@ -57,8 +37,7 @@ export const Login = () => {
           <Input
             type="password"
             name="password"
-            onChange={handleChange}
-            value={password}
+           
           />
         </Label>
         <Button type="submit">Log In</Button>
