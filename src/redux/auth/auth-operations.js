07bb975f752741ad51d2772541a1c06729b5eef3
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-16 00:00:12 +0100

    Update auth-operations.js

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,7 +1,82 @@
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+// // Utility to add JWT
+// const setAuthHeader = token => {
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+
+// // Utility to remove JWT
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
+
+// export const register = createAsyncThunk(
+//   'auth/register',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       const { data } = await axios.post('/users/signup', credentials);
+//       setAuthHeader(data.token);
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const login = createAsyncThunk(
+//   'auth/login',
+//   async (credentials, thunkAPI) => {
+//     try {
+//       const { data } = await axios.post('/users/login', credentials);
+//       setAuthHeader(data.token);
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// // POST - USER LOGOUT - /users/logout
+// // Authorization header  - The token issued to the current user.
+
+// export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+//   try {
+//     await axios.post('/users/logout');
+//     clearAuthHeader();
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.message);
+//   }
+// });
+
+// export const refreshUser = createAsyncThunk(
+//   'auth/refresh',
+//   async (_, thunkAPI) => {
+//     // Reading the token from the state via getState()
+//     const state = thunkAPI.getState();
+//     const persistedToken = state.auth.token;
+
+//     if (persistedToken === null) {
+//       // If there is no token, exit without performing any request
+//       return thunkAPI.rejectWithValue('Unable to donwnload user information');
+//     }
+//     try {
+//       // If there is a token, add it to the HTTP header and perform the request
+//       setAuthHeader(persistedToken);
+//       const { data } = await axios.get('/users/current');
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 // Utility to add JWT
 const setAuthHeader = token => {
@@ -13,11 +88,17 @@ const clearAuthHeader = () => {
   axios.defaults.headers.common.Authorization = '';
 };
 
+/*
+ * POST @ /users/signup
+ * body: { name, email, password }
+ */
 export const register = createAsyncThunk(
   'auth/register',
-  async (_, thunkAPI) => {
+  async ({ email, name, password}, thunkAPI) => {
     try {
-      const { data } = await axios.post('/users/signup');
+      const res = await axios.post('/users/signup', {email,name, password});
+      const data = res.data;
+      // After successful register, add the token to the HTTP header
       setAuthHeader(data.token);
       return data;
     } catch (error) {
@@ -26,31 +107,46 @@ export const register = createAsyncThunk(
   }
 );
 
-export const login = createAsyncThunk(
+/*
+ * POST @ /users/login
+ * body: { email, password }
+ */
+export const logIn = createAsyncThunk(
   'auth/login',
   async (credentials, thunkAPI) => {
     try {
       const { data } = await axios.post('/users/login', credentials);
+      // After successful login, add the token to the HTTP header
       setAuthHeader(data.token);
       return data;
     } catch (error) {
+      // alert("Wrong password or email");
+   
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-// POST - USER LOGOUT - /users/logout
-// Authorization header  - The token issued to the current user.
-
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ */
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await axios.post('/users/logout');
+    // After a successful logout, remove the token from the HTTP header
     clearAuthHeader();
+    // alert("Log out successful");
+
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
 });
 
+/*
+ * GET @ /users/me
+ * headers: Authorization: Bearer token
+ */
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
   async (_, thunkAPI) => {
@@ -60,8 +156,9 @@ export const refreshUser = createAsyncThunk(
 
     if (persistedToken === null) {
       // If there is no token, exit without performing any request
-      return thunkAPI.rejectWithValue('Unable to donwnload user information');
+      return thunkAPI.rejectWithValue('Unable to fetch user');
     }
+
     try {
       // If there is a token, add it to the HTTP header and perform the request
       setAuthHeader(persistedToken);
