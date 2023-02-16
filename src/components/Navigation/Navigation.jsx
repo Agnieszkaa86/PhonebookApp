01b6cb39fd279115b29e64c3a231567a45f522f6
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-16 22:44:31 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,13 +2,12 @@ import { useAuth } from '../../hooks/useAuth';
 import { Nav, Link } from './Navigation.styled';
 
 export const Navigation = () => {
-   const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
   return (
     <Nav style={{ display: 'flex', gap: '10px' }}>
       <Link to="/">Home</Link>
 
-      {isLoggedIn && <Link to="/contacts">My contacts</Link>
-      }
+      {isLoggedIn && <Link to="/contacts">My contacts</Link>}
     </Nav>
   );
 };
