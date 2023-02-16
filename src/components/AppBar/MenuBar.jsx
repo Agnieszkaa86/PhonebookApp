9723e2968e6892e6422cd2b724a380368a2b7a07
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-16 22:44:42 +0100

    Update MenuBar.jsx

--- src/components/AppBar/MenuBar.jsx
+++ src/components/AppBar/MenuBar.jsx
@@ -1,11 +1,11 @@
-import {useAuth} from '../../hooks/useAuth'
+import { useAuth } from '../../hooks/useAuth';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { Container, Header } from './MenuBar.styled';
 
 export const MenuBar = () => {
-const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
   return (
     <div style={{ display: 'block', borderBottom: '1px solid teal' }}>
       <Container>
