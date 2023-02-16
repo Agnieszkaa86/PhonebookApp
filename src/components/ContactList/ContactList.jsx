8e4fe462704bdce85f18cac36376d57e1ffc76cc
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-16 11:00:20 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,14 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   selectLoading,
   selectAllContacts,
-  // selectError,
 } from '../../redux/contacts/selectors';
 import { deleteContact, fetchContacts } from '../../redux/contacts/operations';
 import { List, Item, Button } from './ContactList.styled';
 
 export const ContactList = () => {
   const isLoading = useSelector(selectLoading);
-  // const error = useSelector(selectError);
   const dispatch = useDispatch();
   const contacts = useSelector(selectAllContacts);
 
@@ -21,7 +19,6 @@ export const ContactList = () => {
   return (
     <>
       {isLoading && <p>Data is loading</p>}
-      {/* {error && <p>{error}</p>} */}
       <List>
         {contacts.length > 0 &&
           contacts.map(({ id, name, number }) => {
